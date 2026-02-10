aff8729c17ddfc914e841b4c98c4998560558687
davyrutts <davyrutts@gmail.com>
2026-02-09 18:41:11 -1000

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Army.org/wiki/North_Carolina/husband_needes_wife's.answer/asap",
+    pageTitle: "Army.org/wiki/North_Carolina/husband_needes_wife's.answer/asap-today",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
